925a9ee54171a5e6b2c74fb6b1c6a65aa2ef292d
Candy Chang <candyichang@gmail.com>
2017-06-11 11:18:38 -0700

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ExampleComponent from './ExampleComponent';
 import ThirdComponent from './ThirdComponent';
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import ExampleComponent from './ExampleComponent';
 import './index.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+
+  <Router>
+    <div>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/example">Example</Link></li>
+    </ul>
+
+
+    <Route exact path="/" component={App}/>
+    <Route path="/example" component={ExampleComponent}/>
+    </div>
+  </Router>
+
+
+
+
+  , document.getElementById('root'));
 registerServiceWorker();
